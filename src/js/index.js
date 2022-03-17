75b18e899ce9c1c436fd574c853c3b906b233088
aagraston <aagraston@gmail.com>
2022-03-17 16:49:00 -0700

    working on component overview

--- src/js/index.js
+++ src/js/index.js
@@ -4,4 +4,9 @@ import ReactDOM from 'react-dom'
 import '../scss/app.scss'
 import App from './app'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
